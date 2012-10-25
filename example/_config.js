c34e9a8c8c4feab51777781c5c79bcbc9a4c8e43
olav <olav@bjorkoy.com>
2012-10-25 22:25:12 +0200

    Assets.

---
+++ example/_config.js
@@ -0,0 +1,21 @@
+exports.config = {
+
+  assets: {
+
+    js: {
+      group1: [
+        "js/a.js",
+        "js/b.js"
+      ]
+    },
+
+    css: {
+      group2: [
+        "css/1.css",
+        "css/2.css"
+      ]
+    }
+
+  }
+
+};
