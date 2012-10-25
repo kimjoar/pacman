c7cab7d571cc02be6a73ae708c72582be7fa1420
olav <olav@bjorkoy.com>
2012-10-25 08:19:31 +0200

    init

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -rf example/public
+rm -rf example/files
+mkdir -p example/files
+
+for i in {1..10000}; do
+	echo $i
+    echo "$i" > "example/files/$(echo $i).txt"
+done
+
+for i in {1..100}; do
+	echo $i
+    echo "$i" > "example/files/$(echo $i).html"
+done
