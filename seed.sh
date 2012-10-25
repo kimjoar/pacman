c38cab2f34aef1da5836a5e343eaa41464b0edb4
olav <olav@bjorkoy.com>
2012-10-26 00:44:18 +0200

    Ignore.

--- seed.sh
+++ seed.sh
@@ -5,12 +5,12 @@ rm -rf example/files
 mkdir -p example/files/txt
 mkdir -p example/files/html
 
-for i in {1..5000}; do
+for i in {1..20000}; do
     echo $i
     echo "$i" > "example/files/txt/$(echo $i).txt"
 done
 
-for i in {1..5000}; do
+for i in {1..20000}; do
     echo $i
     echo "$i" > "example/files/html/$(echo $i).html"
 done
