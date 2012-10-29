b8776db71cde419162244c1dd1407750e9990b32
olav <olav@bjorkoy.com>
2012-10-29 15:56:59 +0100

    Refactor.

--- spec/tests/core.spec.js
+++ spec/tests/core.spec.js
@@ -14,24 +14,27 @@ exports.setUp = function(callback) {
   callback();
 };
 
+var f1 = "spec/out/html/1.html";
+var f2 = "spec/out/html/2.html";
+
 exports.canRegenOneFile = function(test) {
   core.regenOne("1.html");
-  test.equal("1.html", fss.readFile("spec/out/html/1.html"));
+  test.equal("1.html", fss.readFile(f1));
   core.regenOne("2.html");
-  test.equal("2.html", fss.readFile("spec/out/html/2.html"));
+  test.equal("2.html", fss.readFile(f2));
   test.done();
 };
 
 exports.canRegenManyFiles = function(test) {
   core.regenAll();
-  test.equal("1.html", fss.readFile("spec/out/html/1.html"));
-  test.equal("2.html", fss.readFile("spec/out/html/2.html"));
+  test.equal("1.html", fss.readFile(f1));
+  test.equal("2.html", fss.readFile(f2));
   test.done();
 };
 
 exports.canRegenType = function(test) {
   core.regenType("html");
-  test.equal("1.html", fss.readFile("spec/out/html/1.html"));
-  test.equal("2.html", fss.readFile("spec/out/html/2.html"));
+  test.equal("1.html", fss.readFile(f1));
+  test.equal("2.html", fss.readFile(f2));
   test.done();
 };
