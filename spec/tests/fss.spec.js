8caa707e270d7608a97bee327ccde777349b4ef1
olav <olav@bjorkoy.com>
2013-01-31 13:06:49 +0100

    Test for copying files.

--- spec/tests/fss.spec.js
+++ spec/tests/fss.spec.js
@@ -1,19 +1,36 @@
+var _      = require("underscore")._;
+var fs     = require("fs");
 var config = require("../../lib/config");
 var fss    = require("../../lib/fss");
-var _      = require("underscore")._;
+var pacman = require("../../lib/pacman");
 
-exports.isDefined = function(test) {
-  test.ok(_.isObject(fss));
+exports.canIngoreUnprocessableFiles = function(test) {
+  config.init({
+    appdir: "spec/cases/filetypes"
+  });
+  fss.resetDir(config.pubdir);
+  test.ok( fss.isProcessableFile("spec/cases/filetypes/_a"), "_a");
+  test.ok( fss.isProcessableFile("spec/cases/filetypes/a"),  "a");
+  test.ok(!fss.isProcessableFile("spec/cases/filetypes/.a"), ".a");
+  test.ok(!fss.isProcessableFile("spec/cases/filetypes"),    "dir");
   test.done();
 };
 
-exports.isProcessableFile = function(test) {
+exports.canCopyZipFiles = function(test) {
   config.init({
-    appdir: "spec/cases/files"
+    appdir: "spec/cases/filetypes",
+    pubdir: "spec/out/filetypes",
+    config: false,
+    layout: false,
+    build: true,
+    dev: false
   });
-  test.ok( fss.isProcessableFile("spec/cases/files/_a"), "_a");
-  test.ok( fss.isProcessableFile("spec/cases/files/a"),  "a");
-  test.ok(!fss.isProcessableFile("spec/cases/files/.a"), ".a");
-  test.ok(!fss.isProcessableFile("spec/cases/files"),    "dir");
+
+  fss.resetDir(config.pubdir);
+  pacman.build();
+
+  test.ok(fs.existsSync("spec/out/filetypes/foo.html"));
+  test.ok(fs.lstatSync("spec/out/filetypes/Archive.zip").isFile());
+  test.ok(fs.lstatSync("spec/out/filetypes/test/test.zip").isFile());
   test.done();
 };
